dc4081f28b6b5cbf5aa125dc21c72647be923424
Logan Smith <logsmith06@gmail.com>
2019-09-28 19:16:06 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,57 +1,39 @@
-def first_challenge
+def third_challenge
    epic_tragedy = {
      :montague => {
        :patriarch => {
          :name => "Lord Montague",
-         :age => 53
+         :age => "53"
        },
        :matriarch => {
          :name => "Lady Montague",
-         :age => 54
+         :age => "54"
        },
        :hero => {
          :name => "Romeo",
-         :age => 15,
+         :age => "15",
          :status => "alive"
        },
        :hero_friends => [
-         {
-           :name => "Benvolio",
-           :age => 17,
-           :attitude => "worried"
-         },
-         {
-           :name => "Mercutio",
-           :age => 18,
-           :attitude => "hot-headed"
-         }
+
        ]
      },
      :capulet => {
        :patriarch => {
          :name => "Lord Capulet",
-         :age => 50
+         :age => "50"
        },
        :matriarch => {
          :name => "Lady Capulet",
-         :age => 51
+         :age => "51"
        },
        :heroine => {
          :name => "Juliet",
-         :age => 15,
+         :age => "15",
          :status => "alive"
        },
        :heroine_friends => [
-         {
-           :name => "Steven",
-           :age => 30,
-           :attitude => "confused"
-         },
-         {
-           :name => "Nurse",
-           :age => 44,
-           :attitude => "worried"
-         }
+
        ]
      }
    }
